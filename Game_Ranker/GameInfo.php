1e6ce565b870c9f995174faa1e75a5ce17d9d4c5
Tebogo11 <33233159+Tebogo11@users.noreply.github.com>
2021-01-15 08:25:55 +0000

    Update GameInfo.php

--- Game_Ranker/GameInfo.php
+++ Game_Ranker/GameInfo.php
@@ -50,38 +50,9 @@
 <?php
 include("functions.php");
 $ID = $_GET["GameID"];
-connect();
 searchByID($ID);
 ?>
-<?php
-if(isset($_SESSION["gatekeeper"])){
-echo '
-<form method="post" action="GameInfo.php?GameID='.$ID.'">
-<div class="mb-3">
-  <label for="commentSection" class="form-label">Comment</label>
-  <textarea class="form-control" id="commentsection" name="comment" rows="3"></textarea>
-</div>
-<button type="submit" class="btn btn-primary">Comment</button>
-</form>';}else
-{
-  echo '<div class="alert alert-warning" role="alert">
-  You have to be <a href="Account/Login.php" class="alert-link">logged in</a> to leave a comment
-</div>';
-}
-?>
-<?php
-$comment = $_POST["comment"];
-$name = $_SESSION["gatekeeper"];
-if($name == null){
-  
-}else{
-  addComment($ID,$comment,$name);
-}
-?>
-<?php
 
-searchComments($ID);
-?>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
