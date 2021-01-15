d404fd069d3d65e89e520697a04810ceb8a1d19b
Tebogo11 <33233159+Tebogo11@users.noreply.github.com>
2021-01-15 08:32:03 +0000

    Update GameInfo.php

--- Game_Ranker/GameInfo.php
+++ Game_Ranker/GameInfo.php
@@ -8,51 +8,41 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">GameRanker</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Ranking.php">Ranking</a>
-        </li>
-        <?php
-        session_start();
-        error_reporting(0);
-        if(!isset($_SESSION["gatekeeper"])){
-          echo '<li class="nav-item">
-          <a class="nav-link" href="Account/Login.php">Login</a>
-        </li>';
-        echo '<li class="nav-item">
-          <a class="nav-link" href="Account/Signup.php">Sign up</a>
-        </li>';
-        }else{
-          echo '<li class="nav-item">
-          <a class="nav-link" href="Account/Logout.php">Log out</a>
-        </li>';
-        }
-        ?>
-      </ul>
-      <form class="d-flex" method="get" action="Search.php">
-        <input class="form-control me-2" type="search" placeholder="Search" name="search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-
 <?php
 include("functions.php");
 $ID = $_GET["GameID"];
+connect();
 searchByID($ID);
 ?>
+<?php
+if(isset($_SESSION["gatekeeper"])){
+echo '
+<form method="post" action="GameInfo.php?GameID='.$ID.'">
+<div class="mb-3">
+  <label for="commentSection" class="form-label">Comment</label>
+  <textarea class="form-control" id="commentsection" name="comment" rows="3"></textarea>
+</div>
+<button type="submit" class="btn btn-primary">Comment</button>
+</form>';}else
+{
+  echo '<div class="alert alert-warning" role="alert">
+  You have to be <a href="Account/Login.php" class="alert-link">logged in</a> to leave a comment
+</div>';
+}
+?>
+<?php
+$comment = $_POST["comment"];
+$name = $_SESSION["gatekeeper"];
+if($name == null){
+  
+}else{
+  addComment($ID,$comment,$name);
+}
+?>
+<?php
 
+searchComments($ID);
+?>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
